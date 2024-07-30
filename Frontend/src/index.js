8d8d8a4ab77eb1aacec213f48e4061155a0072d1
MUSKAAN SINHA <88770752+ENG18CS0178-MUSKAANSINHA@users.noreply.github.com>
2024-07-30 18:10:18 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import './App.css';
+
+ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  document.getElementById('root')
+);
 

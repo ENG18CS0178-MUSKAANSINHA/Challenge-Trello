d30f3be0e25d78c94afa442e33cc8c67186cde8b
MUSKAAN SINHA <88770752+ENG18CS0178-MUSKAANSINHA@users.noreply.github.com>
2024-07-30 17:54:37 +0530

    Update taskModel.js

--- Backend/models/taskModel.js
+++ Backend/models/taskModel.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    column: {
+        type: String,
+        required: true,
+        enum: ['todo', 'in-progress', 'done'],
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
 
